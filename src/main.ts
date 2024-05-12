c9b759d753226e943223b326661ba790a7f0129c
Hoang M Hiep <hoang.minhhiep@outlook.com>
2024-05-13 00:05:50 +0700

    Add OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('FMC demo API')
+    .setDescription('The API for setting up alarm and fire some emails')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const openApiDocument = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api/documentation', app, openApiDocument);
+
   await app.listen(3000);
 }
 bootstrap();
