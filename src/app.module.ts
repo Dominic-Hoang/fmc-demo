503bb44758708bf13859dc386004bc09c4b4f0ea
Hoang M Hiep <hoang.minhhiep@outlook.com>
2024-05-13 09:57:02 +0700

    Config db and oauth2 environment vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().hostname().required(),
+        DB_PORT: Joi.number().port().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        GOOGLE_OAUTH2_CLIENT_ID: Joi.string().required(),
+        GOOGLE_OAUTH2_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_OAUTH2_REDIRECT_URL: Joi.string().uri().required(),
+      }),
+      validationOptions: { abortEarly: true },
+    }),
+  ],
 })
 export class AppModule {}
