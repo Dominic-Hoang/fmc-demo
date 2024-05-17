4178b89ad80a9229d8a7de831bc02a166d51bba1
Hoang M Hiep <hoang.minhhiep@outlook.com>
2024-05-17 17:48:08 +0700

    Refactor distributed lock tests

--- test/scheduler.e2e-spec.ts
+++ test/scheduler.e2e-spec.ts
@@ -25,69 +25,73 @@ describe('MailSchedulerService', () => {
     await app.close();
   });
 
-  const randomProcessObtainLock = async (lockKey: string) => {
-    return await distributedLockService.acquireOrExtendLock(
-      lockKey,
-      randomUUID(),
-      2,
-    );
-  };
+  describe('Distributed lock', () => {
+    const randomProcessObtainLock = async (lockKey: string) => {
+      return await distributedLockService.acquireOrExtendLock(
+        lockKey,
+        randomUUID(),
+        2,
+      );
+    };
 
-  it('Just 1 process obtain lock while 1000 processes try to access at the same time', async () => {
-    const LOCK_KEY = 'test:distributedLock:first';
-    const simulateTasks: Promise<'OK' | null>[] = new Array(1000)
-      .fill(0)
-      .map(() => randomProcessObtainLock(LOCK_KEY));
-    const results = await Promise.all(simulateTasks);
-    let countOkResult = 0;
-    results.forEach((result) => {
-      if (result === 'OK') {
-        countOkResult++;
-      }
+    it('Just 1 process obtain lock while 1000 processes try to access at the same time', async () => {
+      const LOCK_KEY = 'test:distributedLock:first';
+      const simulateTasks: Promise<'OK' | null>[] = new Array(1000)
+        .fill(0)
+        .map(() => randomProcessObtainLock(LOCK_KEY));
+      const results = await Promise.all(simulateTasks);
+      let countOkResult = 0;
+      results.forEach((result) => {
+        if (result === 'OK') {
+          countOkResult++;
+        }
+      });
+
+      expect(countOkResult).toEqual(1);
     });
 
-    expect(countOkResult).toEqual(1);
+    it('The process holding lock able to extend lock TTL', async () => {
+      const LOCK_KEY = 'test:distributedLock:second';
+      const lockHolderValue = randomUUID();
+      const ttlSec = 2;
+      const idleTimeSec = 1;
+      // a process obtain the lock
+      const firstObtain = await distributedLockService.acquireOrExtendLock(
+        LOCK_KEY,
+        lockHolderValue,
+        ttlSec,
+      );
+      expect(firstObtain).toEqual('OK');
+
+      // time passed
+      await new Promise((r) => setTimeout(r, idleTimeSec * 1000));
+
+      // other processes obtain the lock should fail
+      expect(
+        await Promise.all(
+          new Array(10).fill(0).map(() => randomProcessObtainLock(LOCK_KEY)),
+        ),
+      ).toEqual(new Array(10).fill(null));
+
+      // here, the beginning process, again, obtain the lock to extend the TTL
+      const secondObtain = await distributedLockService.acquireOrExtendLock(
+        LOCK_KEY,
+        lockHolderValue,
+        ttlSec,
+      );
+      expect(secondObtain).toEqual('OK');
+
+      // some time passed
+      await new Promise((r) => setTimeout(r, idleTimeSec * 1000));
+
+      // other processes obtain the lock should still fail
+      expect(
+        await Promise.all(
+          new Array(10).fill(0).map(() => randomProcessObtainLock(LOCK_KEY)),
+        ),
+      ).toEqual(new Array(10).fill(null));
+    });
   });
 
-  it('The process holding lock able to extend lock TTL', async () => {
-    const LOCK_KEY = 'test:distributedLock:second';
-    const lockHolderValue = randomUUID();
-    const ttlSec = 2;
-    const idleTimeSec = 1;
-    // a process obtain the lock
-    const firstObtain = await distributedLockService.acquireOrExtendLock(
-      LOCK_KEY,
-      lockHolderValue,
-      ttlSec,
-    );
-    expect(firstObtain).toEqual('OK');
-
-    // time passed
-    await new Promise((r) => setTimeout(r, idleTimeSec * 1000));
-
-    // other processes obtain the lock should fail
-    expect(
-      await Promise.all(
-        new Array(10).fill(0).map(() => randomProcessObtainLock(LOCK_KEY)),
-      ),
-    ).toEqual(new Array(10).fill(null));
-
-    // here, the beginning process, again, obtain the lock to extend the TTL
-    const secondObtain = await distributedLockService.acquireOrExtendLock(
-      LOCK_KEY,
-      lockHolderValue,
-      ttlSec,
-    );
-    expect(secondObtain).toEqual('OK');
-
-    // some time passed
-    await new Promise((r) => setTimeout(r, idleTimeSec * 1000));
-
-    // other processes obtain the lock should still fail
-    expect(
-      await Promise.all(
-        new Array(10).fill(0).map(() => randomProcessObtainLock(LOCK_KEY)),
-      ),
-    ).toEqual(new Array(10).fill(null));
-  });
+  describe('', () => {});
 });
